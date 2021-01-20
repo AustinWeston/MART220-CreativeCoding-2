14f5335c15903cbc79b784de9b2b1b2c35d1dc0e
AustinWeston <36024664+AustinWeston@users.noreply.github.com>
2021-01-19 17:58:30 -0700

    Homework 1

---
+++ MART220-Homework1/Sketch.js
@@ -0,0 +1,43 @@
+/* 
+ MART 220 - Creative Coding 2
+ Author: Austin Cook
+ Purpose: Homework Assignment 1
+ Date: January 19, 2021
+ Here we go again!
+*/
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() {
+  background(10,25,170);
+  fill(40,65,25);
+  triangle(140,40,190,120,240,40);
+  fill(50,30,10);
+  ellipse(60,180,120,50);
+  fill(90, 140, 13);
+  rect(120,90,20,130);
+  fill(80,20,200)
+  rect(260,60,20,40);
+  fill(0,250,25);
+  circle(32,150,60);
+  fill(25,150,20);
+  
+  
+  square(150,190,55);
+  fill(22,47,50);
+  square(250,120,75);
+  fill(0,255,0);
+  rect(83,185,15,30);
+  fill(240,0,200);
+  circle(80,350,60);
+  fill(0,100,250);
+  triangle(250,50,310,90,370,50);
+  fill(0,0,0);
+  text('Austin Cook',285,395);
+  
+
+  
+  
+}
